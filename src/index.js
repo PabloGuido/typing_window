75d3413b317362e5721ce2efd9e8a8d9260157a6
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-04-20 02:06:13 -0300

    La interacción con enemigos empezó a funcionar, se los puede atacar, eliminar y bloquear el camino.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ let palabra = ""
 let numero_de_letra = [0,0,0,0]
 let escribiendo_en = []
 
+// npc
+let npc = []
+
 
 let textEntry_color
 let cambiaColor_sumaLetra
@@ -32,6 +35,7 @@ let keyDel;
 let click
 let del
 let ok
+let hit
 //
 let hero
 let mover_hero
@@ -63,13 +67,30 @@ class Hero
         this.tablaEntry = [this.textEntry0,this.textEntry1,this.textEntry2,this.textEntry3]
         this.tablaEntryC = [this.textEntryC0,this.textEntryC1,this.textEntryC2,this.textEntryC3]
         this.container = esto.add.container(x, y, [ this.sprite, this.textEntry0,this.textEntry1,this.textEntry2,this.textEntry3, this.textEntryC0,this.textEntryC1,this.textEntryC2,this.textEntryC3 ]);
+        this.ataque = 1
+        this.esto = esto
     }  
     mover(dirX, dirY) 
     {
         // console.log(this.container)
         this.container.x = dirX
-        this.container.y = dirY
-        
+        this.container.y = dirY        
+    }
+    atacar(objetivo, esto2){
+        hit.play();
+        objetivo.vida = objetivo.vida - this.ataque
+        console.log(objetivo.nombre + ' vida restante: ' + objetivo.vida)
+        this.esto.tweens.add({
+            targets: objetivo.enemy,
+            scaleX: {value: 4, duration: 75, ease: 'Power0' },
+            // scaleY: {value: 4, duration: 75, ease: 'Power0' },
+            alphaTopLeft: { value: 0.5, duration: 75, ease: 'Power0' },            
+            yoyo: true,
+        });
+        if (objetivo.vida <= 0){
+            objetivo.muerto(npc)
+            console.log(npc)
+        }
     }
 }
 
@@ -91,6 +112,7 @@ class MyGame extends Phaser.Scene
         this.load.audio('click', 'src/assets/click.ogg');
         this.load.audio('del', 'src/assets/del.wav');
         this.load.audio('ok', 'src/assets/ok.ogg');
+        this.load.audio('hit', 'src/assets/hit.wav');
     }
       
     create ()
@@ -105,6 +127,7 @@ class MyGame extends Phaser.Scene
         click = this.sound.add('click', {volume: 0.65});
         del = this.sound.add('del', {volume: 0.65});
         ok = this.sound.add('ok', {volume: 0.45});
+        hit = this.sound.add('hit', {volume: 0.45});
         // Creating a blank tilemap with the specified dimensions
         mapa = this.make.tilemap({ tileWidth: 64, tileHeight: 64, width: 12, height: 12});
         tiles = mapa.addTilesetImage('tiles');
@@ -122,14 +145,26 @@ class MyGame extends Phaser.Scene
         // enemigo
         eTest = new Enemigo(this,100,100)
         this.add.existing(eTest)
-        console.log(eTest.timer)
+        npc.push(eTest)
+        eTest.container.setPosition(layer.layer.data[0][7].pixelX,layer.layer.data[5][0].pixelY)
+
+        let eTest2 = new Enemigo(this,100,100)
+        this.add.existing(eTest2)
+        npc.push(eTest2)
+        eTest2.container.setPosition(layer.layer.data[0][10].pixelX,layer.layer.data[7][0].pixelY)
+        
+        let eTest3 = new Enemigo(this,100,100)
+        this.add.existing(eTest3)
+        npc.push(eTest3)
+        eTest3.container.setPosition(layer.layer.data[0][5].pixelX,layer.layer.data[2][0].pixelY)
+        // console.log(npc)
 
         hero = new Hero(this)
         hero.container.x = layer.layer.data[inicialY][inicialX].pixelX
         hero.container.y= layer.layer.data[inicialY][inicialX].pixelY
         this.cameras.main.startFollow(hero.container, false, 0.2,0.2);
 
-        eTest.container.setPosition(layer.layer.data[0][10].pixelX,layer.layer.data[7][0].pixelY)
+        
         keyEsc = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
         keyDel = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACKSPACE);
         // keyboar input ---
@@ -153,7 +188,7 @@ class MyGame extends Phaser.Scene
                     }
                     if (i === 3){
                         // console.log('3')                        
-                        escribir_letra();
+                        escribir_letra(this);
 
                     }
                    
@@ -178,7 +213,7 @@ class MyGame extends Phaser.Scene
         crear_cuatro_palabras(hero.tablaEntry)
         // console.log(hero.tablaEntry)
         // ----
-
+        console.log(npc)
     }
 
     update()
@@ -223,7 +258,7 @@ crear_cuatro_palabras = function(tabla) {
     // console.log(palabras_a_escribir)
 }
 
-escribir_letra = function () {
+escribir_letra = function (esto2) {
     for (let k = 0; k < 4; k++){
         if (numero_de_letra[k] < Math.max.apply(Math, numero_de_letra)) // compara la cantidad de letras de la palabra contra la palabra con mayor cantidad escrita
         {
@@ -234,15 +269,29 @@ escribir_letra = function () {
         else if(numero_de_letra[k] >= palabras_a_escribir[k].length )
         {   
 
-
+            // Esta primera compración de abajo chequea que no estemos yendo hacia una pared.
             if (layer.layer.data[inicialY + direcciones[k][0]][inicialX].index === 0 && layer.layer.data[inicialY][inicialX + direcciones[k][1]].index === 0){
-                // cambia el incialY/X para llevar tracking de los tiles y comprar que hay cuando se quiera mover.
-                inicialY = inicialY + direcciones[k][0]
-                inicialX = inicialX + direcciones[k][1]  
-                // Mueve al hero.          
+                // Diferencia que se suma la posicion del hero para ver si hay algún NPC o moverse.
                 let x = direcciones[k][1] * 64
                 let y = direcciones[k][0] * 64
-                hero.mover(hero.container.x + x,hero.container.y + y)
+                for (let npcs of npc){
+
+                    if (npcs.container.x === hero.container.x + x && npcs.container.y === hero.container.y + y){
+                        // compara que el hero no vaya a quedar en la misma posición que ya hay un npc.
+                        console.log('Interactuar con npc: ' + npcs.tipo + ' ' + npcs.nombre)
+                        hero.atacar(npcs, esto2)
+                        // ok.play();
+                        palabra_nueva(k);
+                        return
+                    }
+
+                }
+            // cambia el incialY/X para llevar tracking de los tiles y comprar cuando se quiera mover.
+            inicialY = inicialY + direcciones[k][0]
+            inicialX = inicialX + direcciones[k][1]  
+
+            hero.mover(hero.container.x + x,hero.container.y + y)
+
             }
             ok.play();
             palabra_nueva(k);
--- src/scripts/enemigo.js
+++ src/scripts/enemigo.js
@@ -4,10 +4,23 @@ export default class Enemigo extends Phaser.GameObjects.Container
     constructor (scene, x, y)
     {
         super(scene); 
-        this.enemy = scene.add.image(0,0,'enemigo').setScale(3, 3).setOrigin(0,0);
+        this.enemy = scene.add.image(0,0,'enemigo').setScale(3, 3)
+        this.enemy.x = 30
+        this.enemy.y = 30
         this.text = scene.add.text(0, 0, "", {fontSize: '25px', fontStyle: 'bold'}).setPosition(0,-40)
         this.container = scene.add.container(x, y, [this.enemy, this.text ])
+        this.tipo = 'enemigo'
+        this.nombre = 'Medusa'
+        this.vida = 3
         this.timer = 0
+    }
+
+    muerto(tabla){
+        let index = tabla.indexOf(this)
+        tabla.splice(index, 1); 
+        this.container.destroy()
+
+        console.log('muerto')
     }
 
     preUpdate(){
